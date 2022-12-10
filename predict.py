eddf4bfa40d29ee4551ee8f3df4fcacd72730184
nanduladeepak <nanduladeepak@gmail.com>
2022-12-10 16:51:13 +0000

    update

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import tensorflow as tf
+import numpy as np
 
 
 
@@ -8,15 +9,20 @@ data.iloc[:,2:].head()
 
 numeric_cols = ['Store', 'StoreType', 'DayOfWeek', 
               'Promo', 'StateHoliday', 'Season',
-              'Assortment','Promo2']
+              'Assortment','Promo2','CompetitionOpenSinceYear']
 
 dataCleaned = data[numeric_cols]
 
+dataCleaned = dataCleaned.iloc[:].values
+dataCleaned = dataCleaned[:,:-1]
+dataCleaned = dataCleaned.reshape((dataCleaned.shape[0], 1, dataCleaned.shape[1]))
 
-model = tf.keras.models.load_model('classifier_hd')
+
+
+model = tf.keras.models.load_model('rnnTestTanhModel_hd')
 output = model.predict(dataCleaned)
-
+output_denorm = np.exp(output)
 kaggeleOutput = data[['Id']].copy()
-kaggeleOutput['Sales'] = output
+kaggeleOutput['Sales'] = output_denorm
 print(kaggeleOutput)
-kaggeleOutput.to_csv('sample_submission.csv', sep=',', index=False)
+kaggeleOutput.to_csv('sample_submission_tanh.csv', sep=',', index=False)
